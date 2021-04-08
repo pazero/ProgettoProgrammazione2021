40897a97c5f324777f97ff5d3b510190e416fc11
Paolo Ceroni <paoloceroni01@gmail.com>
2021-04-08 10:03:12 +0200

    Add files via upload

---
+++ paolo/errori/ClasseEroe.hpp
@@ -0,0 +1,26 @@
+#include <ncurses.h>
+#include "ClasseOggetto.hpp"
+
+struct Bullet {
+    position pos;
+    Bullet *next;
+};
+typedef Bullet* colpi;
+class Eroe:public Oggetto {
+    protected:
+    int life;
+    char name = 'Y';
+    colpi gun;
+    
+    int nbullet;
+
+    public:
+    Eroe(position pos, int life);
+    void go_up();
+    void go_down();
+    position getPos();
+    int getPosy();
+    int getPosx();
+    void show();
+    void add_bullet(position pos);
+};
