2d3102acf82ace699f6f28e48a23aaf816518ac7
braghira <81851588+braghira@users.noreply.github.com>
2021-04-16 13:18:30 +0200

    Update Lista.hpp

--- Lista.hpp
+++ Lista.hpp
@@ -1,10 +1,11 @@
 #include "Map.hpp"
 #include "Bullet.hpp"
-
+// struttura per la lista bidirezionale
 struct Node
 {
     int n;
-    Map *piece;
+    Map *piece; // "pezzo di mappa": puntatore di tipo Map che crea dei nodi 
+                // con le caratteristiche definite dalla classe Map 
     Bullet  curr;
     Node *next;
     Node *prev;
