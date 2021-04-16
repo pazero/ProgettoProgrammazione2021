1e72ec2e48d36dc9f924e194c8b6e6f515d3ee3b
braghira <81851588+braghira@users.noreply.github.com>
2021-04-16 13:18:52 +0200

    Update Bullet.hpp

--- Bullet.hpp
+++ Bullet.hpp
@@ -1,10 +1,4 @@
 #include "Oggetto.hpp"
-
-/*struct colpo { // struttura per la lista bidirezionale
-    Bullet curr;
-    colpo *next;
-};
-typedef colpo* colpi;*/
 // questa classe crea una finestra 1x1 stampando ">" come proiettile
 class Bullet : public Oggetto{
     protected:
