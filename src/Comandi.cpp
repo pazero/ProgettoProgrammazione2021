2519ba7973f6e69e05eebfb8a1752ebfb9a320dc
Paolo Ceroni <paoloceroni01@gmail.com>
2021-09-01 15:26:22 +0200

    commenti comandi.cpp

--- src/Comandi.cpp
+++ src/Comandi.cpp
@@ -1,9 +1,9 @@
 #include "Comandi.hpp"
 
 Comandi::Comandi() {
-        initscr();
-        noecho();
-        keypad(stdscr, TRUE);
-        curs_set(FALSE);
-        start_color();
+        initscr(); //fa partire la modalità ncurses
+        noecho(); //fa in modo che l'input da tastiera non venga mai stampato a schermo 
+        keypad(stdscr, TRUE); //attiva l'utilizzo della tastiera
+        curs_set(FALSE); //rende invisibile il cursore
+        start_color(); //attiva la modalità a colori
     }
