7b4d1c2127f1dbcf936dd74098e1bba22fda7aca
Paolo Ceroni <paoloceroni01@gmail.com>
2021-09-01 15:26:49 +0200

    commenti comandi.hpp

--- src/Comandi.hpp
+++ src/Comandi.hpp
@@ -1,5 +1,7 @@
 #include <ncurses.h>
 class Comandi {
     public:
+    //questa classe contiene nel suo costruttore solo le funzioni 
+    //necessarie al funzionamento di ncurses
     Comandi();
 };
