6f3950abbb654b5b0e584c6046ba1ba511b7cbce
Paolo Ceroni <paoloceroni01@gmail.com>
2021-09-01 13:16:16 +0200

    commenti enemy.hpp

--- src/ClasseEnemy.hpp
+++ src/ClasseEnemy.hpp
@@ -6,20 +6,15 @@ using namespace std;
 
 #include "ClasseBonus.hpp"
 
-//posizione rispetto a PAD
+//posizione assegnata rispetto al pad e non allo stdscr
 class Enemy:public Oggetto{
     protected:
-    int n;
-    int life;
-
-    int rect_lines;
-    int rect_cols;
-    int delay;
+    int delay;  //intero usato per lo spawn delle corazze dei nemici "A"
 
     public:
     Enemy();
     Enemy(char name);
     
-    void update_pos(position pos);
-    int get_delay();
+    void update_pos(position pos);  //aggiorna la posizione del nemico
+    int get_delay();    //ritorna la variabile "delay"
 };
