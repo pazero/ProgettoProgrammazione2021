e88e6f68768b933ca3cdf09db219885cf7c94ef1
Paolo Ceroni <paoloceroni01@gmail.com>
2021-09-01 13:18:32 +0200

    commenti eroe.hpp

--- src/ClasseEroe.hpp
+++ src/ClasseEroe.hpp
@@ -2,16 +2,16 @@
 #include "ClasseOggetto.hpp"
 class Eroe:public Oggetto {
     protected:
-    int life;
+    int life;   //vita dell'eroe
     
     public:
     Eroe();
     Eroe(position pos, int life);
-    void go_up();
-    void go_down();
-    int getlife();
+    void go_up();   //metodo per salire su una piattaforma
+    void go_down(); //metodo per scendere da una piattaforma
+    int getlife();  //ritorna la vita dell'eroe
 
-    void damage(int danno);
-    void bonus_life();
-    void setlife(int n);
+    void damage(int danno); //infligge danni alla vita dell'eroe
+    void bonus_life();  //metodo necessario al bonus "*": aumenta la vita dell'eroe
+    void setlife(int n);    //imposta "life" come il parametro passato
 };
